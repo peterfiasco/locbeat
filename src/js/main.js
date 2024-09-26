770358c6768c21951cfba4dc4cc9572998124ae2
peterfiasco <peter.okachie@gmail.com>
2024-09-26 09:13:03 +0100

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+@"
+import 'pagedone';
+
+// Add any custom JavaScript here
+"@ | Out-File -FilePath "src\js\main.js" -Encoding utf8
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/pagedone/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
